c99848da5c691795bf690e79900b2d64aef63f9f
chris-poed <christianpoed@gmail.com>
2024-12-17 14:39:59 +0000

    add route path for ArticleView

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@ import './App.css'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import ArticleList from '../components/ArticleList'
+import ArticleView from '../components/ArticleView'
+import {Routes, Route} from 'react-router'
 
 function App () {
 
   return (
     <>
       <Header />
-      <ArticleList />
+      <Routes>
+        <Route path='/' element={<ArticleList/>}/>
+        <Route path='/article/:article_id' element={<ArticleView />}/>
+      </Routes>
       <Footer />
     </>
   )
