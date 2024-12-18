974c4b8c9c212d8b380fd8ba0809c6960d42a289
chris-poed <christianpoed@gmail.com>
2024-12-18 17:35:44 +0000

    create VoteHandler component

--- src/components/VoteHandler.jsx
+++ src/components/VoteHandler.jsx
@@ -1,5 +1,28 @@
+import { useState } from "react"
+import { postVote } from "../utils/api"
+import Button from '@mui/material/Button';
 
+const VoteHandler = ({ article }) => {
+    const [votesAdded, setVotesAdded] = useState(article.votes)
 
-function VoteHandler = () => {
-    
+    const handleClick = () => {
+        postVote(article.article_id).catch(() => {
+            setVotesAdded((currVotesAdded) => {
+                return currVotesAdded -1
+            })
+        })
+        setVotesAdded((currVotesAdded) => {
+            return currVotesAdded + 1
+        })
+    }
+
+    return (
+        <>
+        <Button size="small" onClick={handleClick}>+1 Vote</Button>
+        <p>{article.votes + votesAdded} votes</p>
+        </>
+    )
+
 }
+
+export default VoteHandler
