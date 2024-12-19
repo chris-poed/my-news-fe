dbc129a6fa20bd2b778cb6be902ce795fbe29622
chris-poed <christianpoed@gmail.com>
2024-12-19 10:27:43 +0000

    create reusable VoteHandler component

--- src/components/VoteHandler.jsx
+++ src/components/VoteHandler.jsx
@@ -3,22 +3,35 @@ import { postVote } from "../utils/api"
 import Button from '@mui/material/Button';
 
 const VoteHandler = ({ article }) => {
-    const [votesAdded, setVotesAdded] = useState(article.votes)
+    const [votesAdded, setVotesAdded] = useState(0)
 
-    const handleClick = () => {
-        postVote(article.article_id).catch(() => {
-            setVotesAdded((currVotesAdded) => {
-                return currVotesAdded -1
+    const handleClick = (vote) => {
+
+        if (vote === "inc-vote") {
+            postVote(article.article_id, +1).catch(() => {
+                setVotesAdded((currVotesAdded) => {
+                    return currVotesAdded -1
+                })
             })
-        })
-        setVotesAdded((currVotesAdded) => {
-            return currVotesAdded + 1
-        })
+            setVotesAdded((currVotesAdded) => {
+                return currVotesAdded + 1
+            })
+        } else if (vote === "dec-vote") {
+            postVote(article.article_id, -1).catch(() => {
+                setVotesAdded((currVotesAdded) => {
+                    return currVotesAdded +1
+                })
+            })
+            setVotesAdded((currVotesAdded) => {
+                return currVotesAdded - 1
+            })
+        }
     }
 
     return (
         <>
-        <Button size="small" onClick={handleClick}>+1 Vote</Button>
+        <Button size="small" onClick={() => handleClick("inc-vote")}>+1 Vote</Button>
+        <Button size="small" onClick={() => handleClick("dec-vote")}>-1 Vote</Button>
         <p>{article.votes + votesAdded} votes</p>
         </>
     )
