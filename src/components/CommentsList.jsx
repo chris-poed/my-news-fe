6898c70f30fe334c0a31c51ef6899f13fa214d16
chris-poed <christianpoed@gmail.com>
2024-12-20 13:44:06 +0000

    rerender the comments list on new comment

--- src/components/CommentsList.jsx
+++ src/components/CommentsList.jsx
@@ -1,12 +1,52 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { getComments } from "../utils/api";
-import PostCommentCard from "./PostCommentCard";
+import { UserContext } from "../contexts/UserContext";
+import { useEffect, useState, useContext } from "react";
+import { getComments, postComment } from "../utils/api";
 import Divider from "@mui/material/Divider";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
 
-const CommentsList = ({ comments, article_id, setComments }) => {
+const CommentsList = ({ article_id }) => {
+  const { user } = useContext(UserContext);
+  const [comments, setComments] = useState([]);
+  const [commentToPost, setCommentToPost] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    postComment(article_id, {
+      username: user.username,
+      body: commentToPost,
+    });
+    setCommentToPost("");
+  };
+
+  const handleInput = (e) => {
+    e.preventDefault();
+    setCommentToPost(e.target.value);
+  };
+
+  useEffect(() => {
+    getComments(article_id).then(({ comments }) => {
+      setComments(comments);
+    });
+  }, [handleSubmit]);
+
   return (
     <ul>
+      <form onSubmit={handleSubmit}>
+        <p>Posting as {user.username}:</p>
+        <TextField
+          name="body"
+          fullWidth
+          required
+          id="outlined-required"
+          label="Type your comment"
+          onChange={handleInput}
+          value={commentToPost}
+        />
+        <Button type="submit" size="small">
+          Submit
+        </Button>
+      </form>
       {comments.map((comment) => {
         return (
           <div key={comment.comment_id}>
@@ -19,11 +59,6 @@ const CommentsList = ({ comments, article_id, setComments }) => {
           </div>
         );
       })}
-      <PostCommentCard
-        article_id={article_id}
-        comments={comments}
-        setComments={setComments}
-      />
     </ul>
   );
 };
