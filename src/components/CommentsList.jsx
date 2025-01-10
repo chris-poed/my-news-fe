b8d1495d2febed2a90a4b9bc6c5afa1600608b60
ChristianP-code <christianpoed@gmail.com>
2025-01-10 23:39:02 +0000

    import CommentCard component

--- src/components/CommentsList.jsx
+++ src/components/CommentsList.jsx
@@ -2,18 +2,9 @@ import { useEffect, useState } from "react";
 import { getComments } from "../utils/api";
 import handleSubmit from './CommentsForm'
 import CommentsForm from './CommentsForm'
-import Typography from '@mui/material/Typography'
-import Box from '@mui/material/Box'
-import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
-import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import Avatar from '@mui/material/Avatar';
-import IconButton, { IconButtonProps } from '@mui/material/IconButton';
-import { red } from '@mui/material/colors';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShareIcon from '@mui/icons-material/Share';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
+import CommentCard from './CommentCard'
+import { Container } from "@mui/material";
+import CssBaseline from "@mui/material/CssBaseline";
 
 const CommentsList = ({ article_id }) => {
   const [comments, setComments] = useState([]);
@@ -26,48 +17,11 @@ const CommentsList = ({ article_id }) => {
 
   return (
     <ul>
-      <CommentsForm setComments={setComments} article_id={article_id}/>
-
-      <Card sx={{ maxWidth: 345 }}>
-      <CardHeader
-        avatar={
-          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-            R
-          </Avatar>
-        }
-        action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
-        }
-        title="Shrimp and Chorizo Paella"
-        subheader="September 14, 2016"
-      />
-      <CardContent>
-        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-          This impressive paella is a perfect party dish and a fun meal to cook
-          together with your guests. Add 1 cup of frozen peas along with the mussels,
-          if you like.
-        </Typography>
-      </CardContent>
-      <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
-        <IconButton aria-label="share">
-          <ShareIcon />
-        </IconButton>
-      </CardActions>
-    </Card>
-
+      <CommentsForm setComments={setComments} article_id={article_id} sx={{margin: 10}}/>
 
       {comments.map((comment) => {
         return (
-          <Box key={comment.comment_id} className="comment-card">
-            <Typography variant="caption" sx={{display: 'flex', alignItems: 'flex-start',}}>{comment.author} at {comment.created_at}</Typography>
-            <Typography sx={{display: 'flex', alignItems: 'flex-start',}}>{comment.body}</Typography>
-            <p>{comment.votes} votes</p>
-          </Box>
+          <CommentCard comment={comment} sx={{margin: 30}}/>
         );
       })}
     </ul>
