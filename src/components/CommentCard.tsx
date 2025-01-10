c1a1656cbaa3e9f20ef8141f34ca3b54cfda61d6
ChristianP-code <christianpoed@gmail.com>
2025-01-10 23:38:15 +0000

    create CommentCard component and styling

--- src/components/CommentCard.tsx
+++ src/components/CommentCard.tsx
@@ -2,32 +2,24 @@ import * as React from 'react';
 import { styled } from '@mui/material/styles';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
-import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
-import Collapse from '@mui/material/Collapse';
 import Avatar from '@mui/material/Avatar';
 import IconButton, { IconButtonProps } from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import { red } from '@mui/material/colors';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import ShareIcon from '@mui/icons-material/Share';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 
-interface ExpandMoreProps extends IconButtonProps {
-  expand: boolean;
-}
-
-export default function RecipeReviewCard() {
-  const [expanded, setExpanded] = React.useState(false);
-
+export default function CommentCard({comment}) {
+  const authorFirstLetter = Array.from(comment.author)[0]
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardHeader
+    <Card sx={{ maxWidth: 'sm', mb: 2 }}>
+      <CardHeader authorFirstLetter={authorFirstLetter}
         avatar={
           <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-            R
+            <>{authorFirstLetter}</>
           </Avatar>
         }
         action={
@@ -35,20 +27,20 @@ export default function RecipeReviewCard() {
             <MoreVertIcon />
           </IconButton>
         }
-        title="Shrimp and Chorizo Paella"
-        subheader="September 14, 2016"
+        title={comment.author}
+        subheader={comment.created_at}
       />
       <CardContent>
         <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-          This impressive paella is a perfect party dish and a fun meal to cook
-          together with your guests. Add 1 cup of frozen peas along with the mussels,
-          if you like.
+{comment.body}
         </Typography>
       </CardContent>
       <CardActions disableSpacing>
         <IconButton aria-label="add to favorites">
           <FavoriteIcon />
+
         </IconButton>
+                  <Typography>{comment.votes} votes</Typography>
         <IconButton aria-label="share">
           <ShareIcon />
         </IconButton>
