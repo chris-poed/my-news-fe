1e747bd8a4a751012f1767489b4d0489a1016c62
ChristianP-code <christianpoed@gmail.com>
2025-01-10 23:39:31 +0000

    update styling for CommentsForm as card

--- src/components/CommentsForm.jsx
+++ src/components/CommentsForm.jsx
@@ -3,6 +3,14 @@ import { useContext, useState, useEffect } from "react";
 import { postComment, getComments } from "../utils/api";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import { red } from '@mui/material/colors';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 
 const CommentsForm = ({ article_id, setComments }) => {
 
@@ -29,23 +37,43 @@ const CommentsForm = ({ article_id, setComments }) => {
     setCommentInput(e.target.value);
   };
 
+  const authorFirstLetter = Array.from(user.username)[0]
+
     return (
-        <form onSubmit={handleSubmit}>
-        <p>Posting as {user.username}:</p>
-        <TextField
-          name="body"
-          fullWidth
-          required
-          id="outlined-required"
-          label="Type your comment"
-          onChange={handleInput}
-          value={commentInput}
-        />
-        <Button type="submit" size="small">
-          Submit
-        </Button>
-      </form>
-    )
+      <Card sx={{ maxWidth: 'sm', mb: 2 }}>
+            <CardHeader
+              avatar={
+                <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+                  {authorFirstLetter}
+                </Avatar>
+              }
+              action={
+                <IconButton aria-label="settings">
+                  <MoreVertIcon />
+                </IconButton>
+              }
+              title={user.username}
+            />
+            <CardContent>
+            <form onSubmit={handleSubmit}>
+              <TextField
+                name="body"
+                fullWidth
+                required
+                id="outlined-required"
+                label="Type your comment"
+                onChange={handleInput}
+                value={commentInput}
+              />
+              <Button type="submit" size="small">
+                Submit
+              </Button>
+            </form>
+            </CardContent>
+            <CardActions disableSpacing>
+            </CardActions>
+          </Card>
+          )
 
 }
 
