e983739a6c2500f35e082d94d3574f31a5d7c86b
ChristianP-code <christianpoed@gmail.com>
2025-01-17 23:12:26 +0000

    add AppBar to main app layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,25 @@
-import './App.css'
-import Header from '../src/components/Header'
-import Footer from '../src/components/Footer'
-import ArticleList from '../src/components/ArticleList'
-import ArticleView from '../src/components/ArticleView'
-import {Routes, Route} from 'react-router'
-import Box from '@mui/material/Container'
+import "./App.css";
+import Header from "../src/components/Header";
+import Footer from "../src/components/Footer";
+import ArticleList from "../src/components/ArticleList";
+import ArticleView from "../src/components/ArticleView";
+import { Routes, Route } from "react-router";
+import Box from "@mui/material/Container";
+import AppBar from "./components/AppBar";
 
-function App () {
-
+function App() {
   return (
-    <Box sx={{ display: 'flex', flexDirection: 'column', alignContent: 'center' }} >
-      <Header sx={{ display: 'flex', flexDirection: 'row', alignContent: 'center' }} />
+    <Box
+      sx={{ display: "flex", flexDirection: "column", alignContent: "center" }}
+    >
+      <AppBar />
       <Routes>
-        <Route path='/' element={<ArticleList/>}/>
-        <Route path='/article/:article_id' element={<ArticleView />}/>
+        <Route path="/" element={<ArticleList />} />
+        <Route path="/article/:article_id" element={<ArticleView />} />
       </Routes>
       <Footer />
     </Box>
-  )
+  );
 }
 
-export default App
+export default App;
