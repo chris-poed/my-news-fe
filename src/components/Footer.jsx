cb96d640be9ef1270c4307b4a34b25160ab87c0c
ChristianP-code <christianpoed@gmail.com>
2025-03-02 23:03:58 +0000

    add About Page link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,13 @@
-import { Typography, Box } from "@mui/material";
+import { Typography, Box, Link } from "@mui/material";
 
 const Footer = () => {
+  return (
+    <Box sx={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}>
+      <Link href="/about" underline="hover">
+        {"About this page"}
+      </Link>
+    </Box>
+  );
+};
 
-
-
-    return (
-<Box sx={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
-
-<Typography variant='h5'>
-    Copyright chris-poed 2024
-</Typography>
-
-</Box>
-    )
-}
-
-export default Footer
+export default Footer;
