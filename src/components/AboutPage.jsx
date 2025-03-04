ecb4ccfb208f0d738d391452833bfeab8857596a
ChristianP-code <christianpoed@gmail.com>
2025-03-04 20:22:40 +0000

    update About Page content

--- src/components/AboutPage.jsx
+++ src/components/AboutPage.jsx
@@ -3,11 +3,21 @@ import { Container, Link, Typography } from "@mui/material";
 const AboutPage = () => {
   return (
     <>
-      <Link href="/" underline="hover">
+      <Link href="/" underline="hover" variant="body1">
         {"Back to articles"}
       </Link>
       <Container maxWidth={"sm"}>
-        <Typography variant="body1">About page</Typography>
+        <Typography variant="body1">
+          Welcome to My News - a full-stack community-driven news website to
+          showcase the skills I've learnt on my software development bootcamp
+          with Northcoders.
+          <Link
+            href="https://github.com/chris-poed/my-news-fe"
+            underline="hover"
+          >
+            {"View the GitHub repo."}
+          </Link>
+        </Typography>
       </Container>
     </>
   );
