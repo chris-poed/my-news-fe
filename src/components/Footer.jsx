82f7dfdcc00c0ff8f43a6a5a6310ce88a312ca18
ChristianP-code <christianpoed@gmail.com>
2025-03-04 20:22:55 +0000

    fix Link font

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { Typography, Box, Link } from "@mui/material";
 const Footer = () => {
   return (
     <Box sx={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}>
-      <Link href="/about" underline="hover">
+      <Link href="/about" underline="hover" variant="body1">
         {"About this page"}
       </Link>
     </Box>
