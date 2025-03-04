9dfc46536ced24f47d58e5f15e35e84717f3ff68
ChristianP-code <christianpoed@gmail.com>
2025-03-04 20:36:25 +0000

    update link padding

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,14 @@ import { Typography, Box, Link } from "@mui/material";
 
 const Footer = () => {
   return (
-    <Box sx={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}>
+    <Box
+      sx={{
+        display: "flex",
+        flexWrap: "wrap",
+        justifyContent: "center",
+        marginTop: 5,
+      }}
+    >
       <Link href="/about" underline="hover" variant="body1">
         {"About this page"}
       </Link>
